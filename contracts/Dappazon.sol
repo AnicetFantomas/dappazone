3eff26fe742c24619645df3101b8bb5181156601
AnicetFantomas <anicetmurhula20@gmail.com>
2023-03-08 10:27:45 +0200

    Update Dappazon contract

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,39 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    struct Item {
+        uint id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock
+    }
+
+    mapping (uint256 => Item) public items;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    //list of products
+    function list(
+        uint _id,
+        string memory name,
+        string memory category,
+        string memory image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public {
+        //create Item struct    
+        Item memory item = Item(_id, name, category, image, _cost, _rating, _stock);
+
+        //save Item struct to the blockchain
+        items[_id] = item;
+    }
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,18 @@
-const { expect } = require("chai")
 
+// eslint-disable-next-line no-unused-vars
 const tokens = (n) => {
+  // eslint-disable-next-line no-undef
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappazon", () => {
+    let dappazon;
 
+    beforeEach( async () => {
+      // Deploy the contract
+      const dappazon = await ethers.getContractFactory("Dappazon");
+      // eslint-disable-next-line no-const-assign
+      dappazon = await Dappazon.deploy();
+
+    })  
 })
